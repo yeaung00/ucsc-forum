24733277115ecfd6ffb72527bd9e6683b8e8d8b5
YeWint Aung <ye@YeWints-MBP.attlocal.net>
2023-09-09 16:57:55 -0700

    wrapped middleware with withAuth

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,16 +1,37 @@
 import { getToken } from "next-auth/jwt";
 import { NextRequest, NextResponse } from "next/server";
+import { withAuth } from "next-auth/middleware"
 
-export async function middleware(req: NextRequest, res: NextResponse) {
-  const pathname = req.nextUrl.pathname
-  const isNotLoginPage = !pathname.startsWith('/api/auth')
-  const token = await getToken({ req })
-  
-  console.log('url: ', req.nextUrl.pathname)
-  if (isNotLoginPage && !token) {
-    const url = new URL(`/api/auth/signin`, req.url);
-    url.searchParams.set("callbackUrl", pathname);
-    return NextResponse.redirect(url);
+export default withAuth(
+  async function middleware(req) {
+    const pathname = req.nextUrl.pathname
+    const isNotLoginPage = !pathname.startsWith('/api/auth')
+    const token = await getToken({ req })
+    
+    console.log('url: ', req.nextUrl.pathname)
+    if (isNotLoginPage && !token) {
+      const url = new URL(`/api/auth/signin`, req.url);
+      url.searchParams.set("callbackUrl", pathname);
+      return NextResponse.redirect(url);
+    }
+    return NextResponse.next()
+  },
+  {
+    callbacks: {
+      authorized: ({ token }) => Boolean(token),
+    },
   }
-  return NextResponse.next()
-}
+)
+// export async function middleware(req: NextRequest, res: NextResponse) {
+//   const pathname = req.nextUrl.pathname
+//   const isNotLoginPage = !pathname.startsWith('/api/auth')
+//   const token = await getToken({ req })
+  
+//   console.log('url: ', req.nextUrl.pathname)
+//   if (isNotLoginPage && !token) {
+//     const url = new URL(`/api/auth/signin`, req.url);
+//     url.searchParams.set("callbackUrl", pathname);
+//     return NextResponse.redirect(url);
+//   }
+//   return NextResponse.next()
+// }
