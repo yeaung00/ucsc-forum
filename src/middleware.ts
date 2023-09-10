3560129211726d4aa03aafab8e74524f7862b28d
YeWint Aung <ye@YeWints-MBP.attlocal.net>
2023-09-09 18:56:09 -0700

    fixing login not working on deploy

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,19 +1,21 @@
-import { withAuth } from "next-auth/middleware"
+// import { withAuth } from "next-auth/middleware"
 
-export default withAuth(
-  function middleware (req) {
-  },
-  {
-    callbacks: {
-      authorized: ({ req, token }) => {
-        if (
-          !req.nextUrl.pathname.startsWith('/api/auth/signin') &&
-          token === null
-        ) {
-          return false
-        }
-        return true
-      }
-    }
-  }
-)
+// export default withAuth(
+//   function middleware (req) {
+//   },
+//   {
+//     callbacks: {
+//       authorized: ({ req, token }) => {
+//         return !!token;
+//         // if (
+//         //   !req.nextUrl.pathname.startsWith('/api/auth/signin') &&
+//         //   token === null
+//         // ) {
+//         //   return false
+//         // }
+//         // return true
+//       }
+//     }
+//   }
+// )
+export { default } from "next-auth/middleware"
