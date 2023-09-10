2676433415303f36d8ccbe9de319ba772b32720c
YeWint Aung <ye@YeWints-MBP.attlocal.net>
2023-09-09 17:38:20 -0700

    test

--- next.config.js
+++ next.config.js
@@ -2,7 +2,12 @@
 const nextConfig = {
   experimental: {
     serverActions: true
-  }
+  },
+  env: {
+    GOOGLE_ID: process.env.GOOGLE_ID,
+    GOOGLE_SECRET: process.env.GOOGLE_SECRET,
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET
+  },
 }
 
 module.exports = nextConfig
